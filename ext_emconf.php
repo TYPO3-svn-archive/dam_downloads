a84da45b4a3c6ac932b641bb9810b55340e655e7
andy.grunwald <andy.grunwald@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-30 09:54:16 +0000

    Task #9499: Update ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dam_downloads/trunk@37426 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,42 +1,31 @@
 <?php
-
-########################################################################
-# Extension Manager/Repository config file for ext "dam_downloads".
-#
-# Auto generated 30-08-2010 11:28
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'DAM Download Plugin',
 	'description' => 'This extension provides a frontend plugin to display dam records i.e. for downloads.',
 	'category' => 'plugin',
-	'shy' => 1,
-	'version' => '1.1.0',
+	'version' => '1.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => 'tt_content',
 	'clearcacheonload' => 1,
 	'lockType' => '',
-	'author' => 'Daniel Thomas',
-	'author_email' => 'dt@dpool.net',
-	'author_company' => '',
+	'author' => 'wmdb Systems GmbH',
+	'author_email' => 'peter.kuehn@wmdb.de',
+	'author_company' => 'wmdb Systems GmbH',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '3.5.0-0.0.0',
-			'php' => '3.0.0-0.0.0',
+			'typo3' => '4.2.10-0.0.0',
+			'php' => '5.2.0-0.0.0',
 			'cms' => '',
+			'dam' => '',
 		),
 		'conflicts' => array(
 		),
@@ -45,5 +34,4 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'_md5_values_when_last_written' => 'a:44:{s:12:"ext_icon.gif";s:4:"49ba";s:17:"ext_localconf.php";s:4:"c265";s:14:"ext_tables.php";s:4:"3035";s:14:"ext_tables.sql";s:4:"6b7f";s:28:"ext_typoscript_constants.txt";s:4:"fc09";s:24:"ext_typoscript_setup.txt";s:4:"37cd";s:15:"flexform_ds.xml";s:4:"80fb";s:16:"locallang_db.php";s:4:"9fb9";s:9:"zipit.php";s:4:"4bbc";s:14:"doc/manual.sxw";s:4:"cbec";s:20:"img/btn_end_left.gif";s:4:"358b";s:21:"img/btn_end_right.gif";s:4:"ee01";s:21:"img/btn_next_left.gif";s:4:"d7d9";s:22:"img/btn_next_right.gif";s:4:"3ad2";s:25:"img/btn_next_ten_left.gif";s:4:"faee";s:26:"img/btn_next_ten_right.gif";s:4:"ddb5";s:14:"img/btn_ok.gif";s:4:"71b6";s:14:"img/detail.jpg";s:4:"758d";s:21:"img/icon_download.gif";s:4:"3269";s:24:"img/icons_large/buch.jpg";s:4:"c3aa";s:23:"img/icons_large/doc.jpg";s:4:"3885";s:23:"img/icons_large/exe.jpg";s:4:"f863";s:36:"img/icons_large/flash_player_exe.jpg";s:4:"212a";s:28:"img/icons_large/no_image.jpg";s:4:"8095";s:23:"img/icons_large/pdf.jpg";s:4:"6227";s:23:"img/icons_large/ppt.jpg";s:4:"50b9";s:23:"img/icons_large/swf.jpg";s:4:"e8a3";s:23:"img/icons_large/xls.jpg";s:4:"ca8e";s:23:"img/icons_large/zip.jpg";s:4:"5c29";s:24:"img/icons_small/buch.jpg";s:4:"0638";s:23:"img/icons_small/doc.jpg";s:4:"81f5";s:23:"img/icons_small/exe.jpg";s:4:"791f";s:36:"img/icons_small/flash_player_exe.jpg";s:4:"fd7b";s:28:"img/icons_small/no_image.jpg";s:4:"d810";s:23:"img/icons_small/pdf.jpg";s:4:"7e14";s:23:"img/icons_small/ppt.jpg";s:4:"a042";s:23:"img/icons_small/swf.jpg";s:4:"52d3";s:23:"img/icons_small/xls.jpg";s:4:"ea90";s:23:"img/icons_small/zip.jpg";s:4:"9887";s:14:"pi1/ce_wiz.gif";s:4:"34e9";s:33:"pi1/class.tx_damdownloads_pi1.php";s:4:"eb5d";s:41:"pi1/class.tx_damdownloads_pi1_wizicon.php";s:4:"af2a";s:17:"pi1/locallang.php";s:4:"84d7";s:17:"pi1/template.tmpl";s:4:"1ddb";}',
 );
-
 ?>
